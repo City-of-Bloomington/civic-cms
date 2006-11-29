8b985f5ec1472d486a876226b4df04ff8ed30345
inghamn <inghamn@07107aec-fef2-11de-b9d1-2d015cf73129>
2006-11-29 20:23:38 +0000

    Versioning is now working for Documents
    git-svn-id: https://civic-cms.googlecode.com/svn/trunk@11 07107aec-fef2-11de-b9d1-2d015cf73129

---
+++ html/documents/updateDocument.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @copyright Copyright (C) 2006 City of Bloomington, Indiana. All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.txt
+ */
+/*
+	$_GET variables:	document_id
+*/
+	verifyUser(array('Publisher','Content Creator'));
+
+	if (isset($_GET['document_id']))
+	{
+		$document = new Document($_GET['document_id']);
+		if (!$_SESSION['USER']->canEdit($document))
+		{
+			$_SESSION['errorMessages'][] = "noAccessAllowed";
+			Header("Location: ".BASE_URL."/documents/viewDocument.php?document_id={$document->getId()}");
+			exit();
+		}
+	}
+
+
+
+	if (isset($_POST['document_id']))
+	{
+		$document = new Document($_POST['document_id']);
+
+		if (!$_SESSION['USER']->canEdit($document))
+		{
+			$_SESSION['errorMessages'][] = "noAccessAllowed";
+			Header("Location: ".BASE_URL."/documents/viewDocument.php?document_id={$document->getId()}");
+			exit();
+		}
+
+		$document->setContent($_POST['content']);
+		try
+		{
+			$document->save();
+			Header("Location: ".BASE_URL."/documents/viewDocument.php?document_id={$document->getId()}");
+			exit();
+		}
+		catch (Exception $e) { $_SESSION['errorMessages'][] = $e; }
+	}
+
+	$FCKeditor = new FCKeditor("content");
+	$FCKeditor->BasePath = BASE_URL."/FCKeditor/";
+	$FCKeditor->ToolbarSet = 'Custom';
+	if (isset($document)) { $FCKeditor->Value = $document->getContent(); }
+
+	$template = new Template("transitional");
+	$form = new Block('documents/updateDocumentForm.inc',array('document'=>$document,'FCKeditor'=>$FCKeditor));
+	$template->blocks[] = $form;
+	$template->render();
+?>
