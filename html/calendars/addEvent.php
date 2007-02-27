4a526a0bf48e6574df0bc096bd988e637542838a
inghamn <inghamn@07107aec-fef2-11de-b9d1-2d015cf73129>
2007-02-27 19:03:43 +0000

    Tweaked layout of Event forms.
    git-svn-id: https://civic-cms.googlecode.com/svn/trunk@54 07107aec-fef2-11de-b9d1-2d015cf73129

--- html/calendars/addEvent.php
+++ html/calendars/addEvent.php
@@ -9,9 +9,21 @@
 
 	if (isset($_POST['event']))
 	{
+		# Start and End times are going to come in as strings
+		# They need to be converted to date arrays before setting
+		if (isset($_POST['allDayEvent']))
+		{
+			$_POST['event']['allDayEvent'] = 1;
+			$_POST['startTime'] = '';
+			$_POST['endTime'] = '';
+		}
+		$_POST['event']['start'] = getdate(strtotime(implode(' ',array($_POST['startDate'],$_POST['startTime']))));
+		$_POST['event']['end'] = getdate(strtotime(implode(' ',array($_POST['endDate'],$_POST['endTime']))));
+
 		$event = new Event();
 		foreach($_POST['event'] as $field=>$value)
 		{
+
 			$set = 'set'.ucfirst($field);
 			$event->$set($value);
 		}
--- html/calendars/updateEvent.php
+++ html/calendars/updateEvent.php
@@ -8,6 +8,17 @@
 	if (isset($_GET['event_id'])) { $event = new Event($_GET['event_id']); }
 	if (isset($_POST['event_id']))
 	{
+		# Start and End times are going to come in as strings
+		# They need to be converted to date arrays before setting
+		if (isset($_POST['allDayEvent']))
+		{
+			$_POST['event']['allDayEvent'] = 1;
+			$_POST['startTime'] = '';
+			$_POST['endTime'] = '';
+		}
+		$_POST['event']['start'] = getdate(strtotime(implode(' ',array($_POST['startDate'],$_POST['startTime']))));
+		$_POST['event']['end'] = getdate(strtotime(implode(' ',array($_POST['endDate'],$_POST['endTime']))));
+
 		$event = new Event($_POST['event_id']);
 		foreach($_POST['event'] as $field=>$value)
 		{
