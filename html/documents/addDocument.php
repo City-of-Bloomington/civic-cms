8bba9a993eb9ef76e003ce17f00eb827368bca09
inghamn <inghamn@07107aec-fef2-11de-b9d1-2d015cf73129>
2007-04-09 14:25:17 +0000

    Adding Documents now uses the same Form blocks and process as Updating Documents.
    git-svn-id: https://civic-cms.googlecode.com/svn/trunk@84 07107aec-fef2-11de-b9d1-2d015cf73129

--- html/documents/addDocument.php
+++ html/documents/addDocument.php
@@ -14,30 +14,56 @@
 		if (!$section->permitsEditingBy($_SESSION['USER'])) { unset($section); }
 	}
 
+	# Set the current language we're working with
+	$language = isset($_REQUEST['lang']) ? new Language($_REQUEST['lang']) : new Language($_SESSION['LANGUAGE']);
+
 	# Create the new, empty document
-	$document = new Document();
-	if (isset($_GET['documentType_id']))
+	if (!isset($_SESSION['document']))
 	{
-		$document->setDocumentType_id($_GET['documentType_id'],$_SESSION['LANGUAGE']);
-		$document->setTitle("New {$document->getDocumentType()}");
+		$_SESSION['document'] = new Document();
+		if (isset($section)) { $_SESSION['document']->addSection($section); }
+		if (isset($_GET['documentType_id']))
+		{
+			$_SESSION['document']->setDocumentType_id($_GET['documentType_id'],$_SESSION['LANGUAGE']);
+			$_SESSION['document']->setTitle("New {$_SESSION['document']->getDocumentType()}");
+		}
 	}
 
-	# If they've posted, populate the document with all their stuff
+
+
+
+	# Handle any document data that's been posted
 	if (isset($_POST['document']))
 	{
-		$document = new Document();
-		$language = new Language($_POST['lang']);
 		foreach($_POST['document'] as $field=>$value)
 		{
 			$set = "set".ucfirst($field);
-			$document->$set($value);
+			$_SESSION['document']->$set($value);
 		}
-		$document->setContent($_POST['content'],$language->getCode());
-		$document->setDocumentType_id($_POST['documentType_id'],$language->getCode());
-
+	}
+	# Content has to be handled specially
+	if (isset($_POST['content'])) { $_SESSION['document']->setContent($_POST['content'],$language->getCode()); }
+	/*
+	 Attachments cannot be created until we have a document_id.
+	 Which means we cannot do attachments until we've saved the document.
+	 Attachments cannot be done while adding a document, only when updating.
+	*/
+	# Raw source code handling
+	if (isset($_FILES['source']) && $_FILES['source']['name'])
+	{
+		# Make sure they're allowed to edit the raw source code
+		if (userHasRole('Webmaster'))
+		{
+			$_SESSION['document']->setContent(file_get_contents($_FILES['source']['tmp_name']),$_POST['lang']);
+		}
+	}
+	# Save the document only when they ask for it
+	if (isset($_POST['action']) && $_POST['action']=='save')
+	{
 		try
 		{
-			$document->save();
+			$_SESSION['document']->save();
+			unset($_SESSION['document']);
 			$template = new Template('closePopup');
 			$template->render();
 			exit();
@@ -46,13 +72,39 @@
 	}
 
 
-	$form = new Block('documents/addDocumentForm.inc');
-	$form->document = $document;
-	$form->language = isset($language) ? $language : new Language($_SESSION['LANGUAGE']);
-	if (isset($section)) { $form->section = $section; }
-	$form->sectionList = new SectionList(array('postable_by'=>$_SESSION['USER']));
 
+
+	/*
+		Since we cannot do attachments while adding a document, we need to use a
+		different set of tabs.  However, we can still use the same form blocks
+		used when updating documents.  Just make sure to not handle attachments.
+	*/
+	# Figure out which tab we're supposed to show
+	$tab = isset($_REQUEST['tab']) ? $_REQUEST['tab'] : 'info';
 	$template = new Template('popup');
+	$template->blocks[] = new Block('documents/add/tabs.inc');
+
+	$form = new Block("documents/update/$tab.inc",array('document'=>$_SESSION['document']));
+	# Handle any extra data the current tab needs
+	switch ($tab)
+	{
+		case 'content':
+			$form->language = $language;
+		break;
+
+		case 'sections':
+			$form->sectionList = new SectionList(array('postable_by'=>$_SESSION['USER']));
+		break;
+
+		case 'source':
+			# Make sure they're allowed to edit the raw source code
+			if (!userHasRole('Webmaster')) { $form = new Block('documents/udpate/info.inc',array('document'=>$_SESSION['document'])); }
+			$form->language = $language;
+		break;
+
+		default:
+	}
+
 	$template->blocks[] = $form;
 	$template->render();
 ?>
