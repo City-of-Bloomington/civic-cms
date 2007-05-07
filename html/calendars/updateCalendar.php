f7d45ac14ea47e840d8f78cc4b0017fb868acd14
inghamn <inghamn@07107aec-fef2-11de-b9d1-2d015cf73129>
2007-05-07 14:03:20 +0000

    Calendars can now have multiple departments
    git-svn-id: https://civic-cms.googlecode.com/svn/trunk@98 07107aec-fef2-11de-b9d1-2d015cf73129

--- html/calendars/updateCalendar.php
+++ html/calendars/updateCalendar.php
@@ -4,7 +4,7 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.txt
  * @author Cliff Ingham <inghamn@bloomington.in.gov>
  */
-	verifyUser('Webmaster');
+	verifyUser(array('Administrator','Webmaster'));
 
 	if (isset($_GET['calendar_id'])) { $calendar = new Calendar($_GET['calendar_id']); }
 	if (isset($_POST['calendar_id']))
