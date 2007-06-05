57cfa89d024e91d75b03c75def581a933ecacbb4
inghamn <inghamn@07107aec-fef2-11de-b9d1-2d015cf73129>
2007-06-05 15:18:22 +0000

    You can now remove Parents from Sections.
    git-svn-id: https://civic-cms.googlecode.com/svn/trunk@147 07107aec-fef2-11de-b9d1-2d015cf73129

--- html/sections/removeParent.php
+++ html/sections/removeParent.php
@@ -3,5 +3,14 @@
  * @copyright Copyright (C) 2007 City of Bloomington, Indiana. All rights reserved.
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.txt
  * @author Cliff Ingham <inghamn@bloomington.in.gov>
+ * @param GET sectionNode_id
  */
+	verifyUser(array('Administrator','Webmaster'));
+
+	$node = new SectionNode($_GET['sectionNode_id']);
+	$section_id = $node->getSection_id();
+
+	$node->delete();
+
+	Header("Location: viewSection.php?section_id=$section_id");
 ?>
