102ae25b3d71fe93a03b90b2e0f6bdbceac1ce18
inghamn <inghamn@07107aec-fef2-11de-b9d1-2d015cf73129>
2007-07-17 15:43:02 +0000

    You can now completely delete media from the system
    git-svn-id: https://civic-cms.googlecode.com/svn/trunk@210 07107aec-fef2-11de-b9d1-2d015cf73129

---
+++ html/media/deleteMedia.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @copyright Copyright (C) 2007 City of Bloomington, Indiana. All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.txt
+ * @author Cliff Ingham <inghamn@bloomington.in.gov>
+ * @param GET media_id
+ */
+	verifyUser(array('Administrator','Webmaster'));
+
+	$media = new Media($_GET['media_id']);
+	$media->delete();
+
+	Header('Location: attachments.php');
+?>
