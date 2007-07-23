0539599084c857e124d6e38de8750d9859f016da
inghamn <inghamn@07107aec-fef2-11de-b9d1-2d015cf73129>
2007-07-23 14:45:22 +0000

    Switched all generated links to Documents to use $document->getURL()
    git-svn-id: https://civic-cms.googlecode.com/svn/trunk@225 07107aec-fef2-11de-b9d1-2d015cf73129

--- html/documents/removeWatch.php
+++ html/documents/removeWatch.php
@@ -1,12 +1,11 @@
 <?php
 /**
- * @copyright Copyright (C) 2006 City of Bloomington, Indiana. All rights reserved.
+ * @copyright Copyright (C) 2006,2007 City of Bloomington, Indiana. All rights reserved.
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.txt
+ * @author Cliff Ingham <inghamn@bloomington.in.gov>
+ * @param GET document_id
  */
-/*
-	$_GET variables:	document_id
-*/
 	$document = new Document($_GET['document_id']);
 	$document->removeWatch($_SESSION['USER']);
-	Header("Location: viewDocument.php?document_id={$document->getId()}");
+	Header('Location: '.$document->getURL());
 ?>
