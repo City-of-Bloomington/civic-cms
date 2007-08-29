a360806e70e9bd847edab1521080fccf587386a6
inghamn <inghamn@07107aec-fef2-11de-b9d1-2d015cf73129>
2007-08-29 15:38:06 +0000

    Renamed and reformatted document blocks:  Siblings, Featured, and Subsections Fixed a bug where the More button was getting displayed twice
    git-svn-id: https://civic-cms.googlecode.com/svn/trunk@340 07107aec-fef2-11de-b9d1-2d015cf73129

--- html/documents/viewDocument.php
+++ html/documents/viewDocument.php
@@ -50,7 +50,12 @@
 					$documentList = new DocumentList(array('documentType_id'=>$type->getId(),'section_id'=>$s->getId(),'featured'=>1,'active'=>date('Y-m-d')));
 					if (count($documentList))
 					{
-						$template->blocks[] = new Block('sections/featuredDocuments.inc',array('documentType'=>$type,'documentList'=>$documentList));
+						$featuredDocuments = new Block('sections/featuredDocuments.inc');
+						$featuredDocuments->documentType = $type;
+						$featuredDocuments->documentList = $documentList;
+						$featuredDocuments->section = $s;
+
+						$template->blocks[] = $featuredDocuments;
 					}
 				}
 			}
