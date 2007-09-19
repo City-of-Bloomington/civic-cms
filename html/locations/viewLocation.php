83b2c1289d5b53676672b48526f1246794365706
inghamn <inghamn@07107aec-fef2-11de-b9d1-2d015cf73129>
2007-09-19 17:17:24 +0000

    Created a page to display one single location
    git-svn-id: https://civic-cms.googlecode.com/svn/trunk@407 07107aec-fef2-11de-b9d1-2d015cf73129

---
+++ html/locations/viewLocation.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @copyright Copyright (C) 2007 City of Bloomington, Indiana. All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.txt
+ * @author Cliff Ingham <inghamn@bloomington.in.gov>
+ * @param GET location_id
+ */
+	$location = new Location($_GET['location_id']);
+
+	$template = new Template();
+	$template->blocks[] = new Block('locations/viewLocation.inc',array('location'=>$location));
+	$template->render();
+?>
