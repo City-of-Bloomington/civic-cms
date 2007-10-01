60fe94f4443f75ca270017c45917a1efeb2d7bc8
inghamn <inghamn@07107aec-fef2-11de-b9d1-2d015cf73129>
2007-10-01 19:55:31 +0000

    Created a new Skin Chooser page
    git-svn-id: https://civic-cms.googlecode.com/svn/trunk@422 07107aec-fef2-11de-b9d1-2d015cf73129

---
+++ html/skins/home.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @copyright Copyright (C) 2007 City of Bloomington, Indiana. All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.txt
+ * @author Cliff Ingham <inghamn@bloomington.in.gov>
+ * @param GET skin
+ *
+ * Passing a skin name as a parameter in the URL should be handled
+ * by /configuration.inc.  So all we need to do is list the skins with an HREF
+ */
+	$template = new Template('backend');
+	$template->blocks[] = new Block('skins/skinList.inc');
+	$template->render();
