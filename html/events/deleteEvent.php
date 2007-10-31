74c1337b2c62ccd89d39c5024132f00ea9c718c5
inghamn <inghamn@07107aec-fef2-11de-b9d1-2d015cf73129>
2007-10-31 19:05:10 +0000

    Events are now deletable
    git-svn-id: https://civic-cms.googlecode.com/svn/trunk@458 07107aec-fef2-11de-b9d1-2d015cf73129

---
+++ html/events/deleteEvent.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @copyright Copyright (C) 2007 City of Bloomington, Indiana. All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.txt
+ * @author Cliff Ingham <inghamn@bloomington.in.gov>
+ * @param GET event_id
+ */
+	verifyUser();
+
+	$event = new Event($_GET['event_id']);
+	if ($event->permitsEditingBy($_SESSION['USER']))
+	{
+		$event->delete();
+	}
+	Header('Location: ../calendars');
