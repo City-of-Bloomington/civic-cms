a6911467cbb873b884d648b1603518f853b971ec
inghamn <inghamn@07107aec-fef2-11de-b9d1-2d015cf73129>
2007-11-07 14:33:11 +0000

    Created a Feeds page.  This page can serve as a routing point for feeds that need a special protocol that the browser doesn't understand.
    git-svn-id: https://civic-cms.googlecode.com/svn/trunk@465 07107aec-fef2-11de-b9d1-2d015cf73129

---
+++ html/feeds/home.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @copyright Copyright (C) 2007 City of Bloomington, Indiana. All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.txt
+ * @author Cliff Ingham <inghamn@bloomington.in.gov>
+ * @param GET type
+ * @param GET url
+ */
+	$template = new Template();
+
+	if (isset($_GET['url']))
+	{
+		$url = new URL($_GET['url']);
+		$handler = new Block('feeds/handler.inc');
+		$handler->url = new URL($_GET['url']);
+		$handler->type = $_GET['type'];
+		$template->blocks[] = $handler;
+	}
+
+	$template->blocks[] = new Block('feeds/rss.inc');
+	$template->blocks[] = new Block('feeds/ical.inc');
+	$template->blocks[] = new Block('feeds/kml.inc');
+	$template->render();
