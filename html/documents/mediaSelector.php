764917d78b1d912e3eb1ecb16111cb90b2046c9b
inghamn <inghamn@07107aec-fef2-11de-b9d1-2d015cf73129>
2007-11-30 15:53:35 +0000

    Unified the Media list and the Attachment Browser (AKA documents/mediaSelector)  Code is mostly the same between the two, however they are not using the same includes.
    git-svn-id: https://civic-cms.googlecode.com/svn/trunk@498 07107aec-fef2-11de-b9d1-2d015cf73129

--- html/documents/mediaSelector.php
+++ html/documents/mediaSelector.php
@@ -4,13 +4,49 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.txt
  * @author Cliff Ingham <inghamn@bloomington.in.gov>
  */
-	verifyUser(array('Administrator','Webmaster','Content Creator','Publisher'));
+verifyUser(array('Administrator','Webmaster','Content Creator','Publisher'));
 
-	$department_id = isset($_GET['department_id']) ? $_GET['department_id'] : $_SESSION['USER']->getDepartment_id();
-	$department = new Department($department_id);
-	$mediaList = new MediaList(array('department_id'=>$department_id));
+# We want the user to always be in their department by default
+$department = isset($_GET['department_id']) ? new Department($_GET['department_id']) : $_SESSION['USER']->getDepartment();
 
-	$template = new Template('mediaBrowser');
-	$template->blocks[] = new Block('documents/mediaSelector.inc',array('mediaList'=>$mediaList,'department'=>$department));
-	$template->render();
-?>
+# Check for any filters the user has applied
+$fields = array();
+$fields['department_id'] = $department->getId();
+if (isset($_GET['mime_type']) && $_GET['mime_type']) { $fields['mime_type'] = $_GET['mime_type']; }
+if (!count($fields)) { $fields = null; }
+
+# Check for the sorting the user has applied
+$sort = isset($_GET['sort']) ? urldecode($_GET['sort']) : null;
+
+# Get the list of media
+$mediaList = new MediaList();
+$mediaList->find($fields,$sort);
+
+# For long lists, paginate the results
+if (count($mediaList) > 50)
+{
+	$pages = $mediaList->getPagination(50);
+
+	# Make sure we're asking for a page that actually exists
+	$page = (isset($_GET['page']) && $_GET['page']) ? (int)$_GET['page'] : 0;
+	if (!$pages->offsetExists($page)) { $page = 0; }
+
+	$media = new LimitIterator($mediaList->getIterator(),$pages[$page],$pages->getPageSize());
+}
+else { $media = $mediaList; }
+
+
+
+# Render the page
+$template = new Template('mediaBrowser');
+$template->blocks[] = new Block('documents/mediaSelector.inc',array('mediaList'=>$media,'department'=>$department));
+if (isset($pages))
+{
+	$pageNavigation = new Block('pageNavigation.inc');
+	$pageNavigation->page = $page;
+	$pageNavigation->pages = $pages;
+	$pageNavigation->url = new URL("$_SERVER[SERVER_NAME]$_SERVER[REQUEST_URI]");
+
+	$template->blocks[] = $pageNavigation;
+}
+$template->render();
