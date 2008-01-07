6a9b5c374efe5c20da2396582ad2bac28ca7fb0b
inghamn <inghamn@07107aec-fef2-11de-b9d1-2d015cf73129>
2008-01-07 21:17:14 +0000

    Images displayed by the server are now a Medium size by default.  Currently set to 350px.
    git-svn-id: https://civic-cms.googlecode.com/svn/trunk@558 07107aec-fef2-11de-b9d1-2d015cf73129

--- html/media/media.php
+++ html/media/media.php
@@ -4,35 +4,43 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.txt
  * @author Cliff Ingham <inghamn@bloomington.in.gov>
  * @param GET media_id
+ * @param GET size (optional for Images)
  *
  * Script to serve all media
  */
- 	if ($_GET['media_id'])
- 	{
-		try
-		{
-			$media = new Media($_GET['media_id']);
-
-			$mime = $media->getMime_type();
-			$disposition = $media->getMedia_type()=='attachment' ? 'attachment' : 'inline';
-			$filename = $media->getFilename();
-			$path = $media->getDirectory();
-			$internalFilename = $media->getInternalFilename();
-		}
-		catch(Exception $e) { }
-	}
-
-	if (!isset($media))
+if ($_GET['media_id'])
+{
+	try
 	{
-		$mime = 'image/png';
-		$disposition = 'inline';
-		$filename = 'missing.png';
-		$path = APPLICATION_HOME.'/html/media';
-		$internalFilename = $filename;
+		$media = new Media($_GET['media_id']);
+
+		$mime = $media->getMime_type();
+		$disposition = $media->getMedia_type()=='attachment' ? 'attachment' : 'inline';
+		$filename = $media->getFilename();
+		$path = $media->getDirectory();
+		$internalFilename = $media->getInternalFilename();
 	}
-	Header('Expires: 0');
-	Header('Pragma: cache');
-	Header('Cache-Control: private');
-	Header("Content-type: $mime");
-	Header("Content-Disposition: $disposition; filename=$filename");
-	readfile("$path/$internalFilename");
+	catch(Exception $e) { }
+}
+
+if (!isset($media))
+{
+	$mime = 'image/png';
+	$disposition = 'inline';
+	$filename = 'missing.png';
+	$path = APPLICATION_HOME.'/html/media';
+	$internalFilename = $filename;
+}
+Header('Expires: 0');
+Header('Pragma: cache');
+Header('Cache-Control: private');
+Header("Content-type: $mime");
+Header("Content-Disposition: $disposition; filename=$filename");
+
+if ($media->getMedia_type() == 'image')
+{
+	$size = (isset($_GET['size'])&&$_GET['size']) ? $_GET['size'] : 'medium';
+	$image = new Image($media->getId());
+	$image->output($size);
+}
+else { readfile("$path/$internalFilename"); }
