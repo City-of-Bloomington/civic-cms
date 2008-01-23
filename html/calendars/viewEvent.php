4f637a701ea5ca3ecb30ef6ed0187709055d9354
inghamn <inghamn@07107aec-fef2-11de-b9d1-2d015cf73129>
2008-01-23 20:55:41 +0000

    View Event now supports specifying which date you want to view the event for.  Given a date, it will try and find the occurence of that event on that date.  If it finds an occurence, it won't display any RRULE stuff, it only shows that occurence's info.  If it doesn't find an occurence, it errors out and displays the FULL event info.
    git-svn-id: https://civic-cms.googlecode.com/svn/trunk@611 07107aec-fef2-11de-b9d1-2d015cf73129

--- html/calendars/viewEvent.php
+++ html/calendars/viewEvent.php
@@ -4,18 +4,34 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.txt
  * @author Cliff Ingham <inghamn@bloomington.in.gov>
  * @param GET event_id
+ * @param GET date (optional) A date in the form of Y-m-d
  */
-	$event = new Event($_GET['event_id']);
+$event = new Event($_GET['event_id']);
 
-	$date = getdate($event->getStart());
-	$url = new URL(BASE_URL."/calendars");
-	$url->parameters = array('year'=>$date['year'],'mon'=>$date['mon'],'mday'=>$date['mday']);
+$date = getdate($event->getStart());
+$url = new URL(BASE_URL."/calendars");
+$url->parameters = array('year'=>$date['year'],'mon'=>$date['mon'],'mday'=>$date['mday']);
 
-	$calendar = $event->getCalendar();
+$calendar = $event->getCalendar();
 
-	$template = new Template();
-	$template->blocks[] = new Block('calendars/breadcrumbs.inc',array('event'=>$event));
-	$template->blocks[] = new Block('calendars/viewButtons.inc',array('url'=>$url,'calendar'=>$calendar));
-	$template->blocks[] = new Block('events/viewEvent.inc',array('event'=>$event));
-	$template->render();
-?>
+$template = new Template();
+$template->blocks[] = new Block('calendars/breadcrumbs.inc',array('event'=>$event));
+$template->blocks[] = new Block('calendars/viewButtons.inc',array('url'=>$url,'calendar'=>$calendar));
+
+
+# If a specific date is requested, load the first recurrence
+# of that event for that date
+if (isset($_GET['date']))
+{
+	$rangeStart = strtotime($_GET['date']);
+	$rangeEnd = strtotime('+1 day',$rangeStart);
+
+	$recurrences = $event->getRecurrences($rangeStart,$rangeEnd);
+	if (count($recurrences))
+	{
+		$event = $recurrences[0];
+	}
+	else { $_SESSION['errorMessages'][] = new Exception('events/unknownRecurrence'); }
+}
+$template->blocks[] = new Block('events/viewEvent.inc',array('event'=>$event));
+$template->render();
