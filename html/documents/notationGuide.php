b00469731fb6a7b05f3eca992f5e710796cbe4e8
inghamn <inghamn@07107aec-fef2-11de-b9d1-2d015cf73129>
2008-01-24 22:26:32 +0000

    Added new documentation for the documents/notationGuide
    git-svn-id: https://civic-cms.googlecode.com/svn/trunk@620 07107aec-fef2-11de-b9d1-2d015cf73129

--- html/documents/notationGuide.php
+++ html/documents/notationGuide.php
@@ -4,6 +4,26 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.txt
  * @author Cliff Ingham <inghamn@bloomington.in.gov>
  */
+$page = isset($_GET['page']) ? $_GET['page'] : '';
+switch ($page)
+{
+	case 'attachments':
+		$guide = new Block('documents/notationGuide/attachments.inc');
+	break;
+
+	case 'facets':
+		$guide = new Block('documents/notationGuide/facets.inc');
+	break;
+
+	case 'images':
+		$guide = new Block('documents/notationGuide/images.inc');
+	break;
+
+	default:
+		$guide = new Block('documents/notationGuide/links.inc');
+}
+
 $template = new Template('popup');
-$template->blocks[] = new Block('documents/notationGuide.inc');
+$template->blocks[] = new Block('documents/notationGuide/toc.inc');
+$template->blocks[] = $guide;
 $template->render();
