dde141753a36ab7ddb9bad9053818805c90aa6ec
inghamn <inghamn@07107aec-fef2-11de-b9d1-2d015cf73129>
2008-02-15 18:43:05 +0000

    User list no longer errors out with the public users.  We've seperated the display by AuthenticationMethod.  Right now, we're only displaying the LDAP users.
    git-svn-id: https://civic-cms.googlecode.com/svn/trunk@660 07107aec-fef2-11de-b9d1-2d015cf73129

--- html/users/home.php
+++ html/users/home.php
@@ -1,16 +1,21 @@
 <?php
 /**
- * @copyright Copyright (C) 2006,2007 City of Bloomington, Indiana. All rights reserved.
+ * @copyright Copyright (C) 2006,2007,2008 City of Bloomington, Indiana. All rights reserved.
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.txt
  * @author Cliff Ingham <inghamn@bloomington.in.gov>
+ * @param GET authenticationMethod
  */
-	verifyUser('Administrator');
+verifyUser('Administrator');
 
-	$userList = new UserList();
-	$userList->find();
+$authenticationMethod = isset($_GET['authenticationMethod']) ? $_GET['authenticationMethod'] : 'LDAP';
 
-	$template = isset($_GET['format']) ? new Template($_GET['format'],$_GET['format']) : new Template();
-	$template->blocks[] = new Block('users/userList.inc',array('userList'=>$userList));
+$users = new UserList();
+$users->find(array('authenticationMethod'=>$authenticationMethod));
+$userList = new Block('users/userList.inc');
+$userList->userList = $users;
+$userList->title = "$authenticationMethod Users";
 
-	$template->render();
-?>
+
+$template = isset($_GET['format']) ? new Template($_GET['format'],$_GET['format']) : new Template();
+$template->blocks[] = $userList;
+$template->render();
