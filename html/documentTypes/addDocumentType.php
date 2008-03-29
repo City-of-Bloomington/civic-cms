fc1f41b54d4faffc8923a99044e719a569d7fc43
inghamn <inghamn@07107aec-fef2-11de-b9d1-2d015cf73129>
2008-03-29 18:07:00 +0000

    Document Info Fields for documentTypes are now orderable.  So, for example, you can specify that, for 'News' documents, the Date comes first, then the Title.
    git-svn-id: https://civic-cms.googlecode.com/svn/trunk@799 07107aec-fef2-11de-b9d1-2d015cf73129

--- html/documentTypes/addDocumentType.php
+++ html/documentTypes/addDocumentType.php
@@ -1,30 +1,36 @@
 <?php
 /**
- * @copyright Copyright (C) 2006 City of Bloomington, Indiana. All rights reserved.
+ * @copyright Copyright (C) 2006-2008 City of Bloomington, Indiana. All rights reserved.
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.txt
  * @author Cliff Ingham <inghamn@bloomington.in.gov>
  */
-	verifyUser(array('Administrator','Webmaster'));
+verifyUser(array('Administrator','Webmaster'));
 
-	if (isset($_POST['documentType']))
+if (isset($_POST['documentType']))
+{
+	$documentType = new DocumentType();
+	foreach($_POST['documentType'] as $field=>$value)
 	{
-		$documentType = new DocumentType();
-		foreach($_POST['documentType'] as $field=>$value)
-		{
-			$set = "set".ucfirst($field);
-			$documentType->$set($value);
-		}
-
-		try
-		{
-			$documentType->save();
-			Header("Location: home.php");
-			exit();
-		}
-		catch (Exception $e) { $_SESSION['errorMessages'][] = $e; }
+		$set = "set".ucfirst($field);
+		$documentType->$set($value);
 	}
 
-	$template = new Template();
-	$template->blocks[] = new Block('documentTypes/addDocumentTypeForm.inc');
-	echo $template->render();
-?>
+	$documentInfoFields = array();
+	foreach($_POST['documentInfoFields'] as $field=>$order)
+	{
+		if ($order) { $documentInfoFields[$order] = $field; }
+	}
+	$documentType->setDocumentInfoFields($documentInfoFields);
+
+	try
+	{
+		$documentType->save();
+		Header("Location: home.php");
+		exit();
+	}
+	catch (Exception $e) { $_SESSION['errorMessages'][] = $e; }
+}
+
+$template = new Template();
+$template->blocks[] = new Block('documentTypes/addDocumentTypeForm.inc');
+echo $template->render();
--- html/documentTypes/updateDocumentType.php
+++ html/documentTypes/updateDocumentType.php
@@ -1,32 +1,37 @@
 <?php
 /**
- * @copyright Copyright (C) 2006 City of Bloomington, Indiana. All rights reserved.
+ * @copyright Copyright (C) 2006-2008 City of Bloomington, Indiana. All rights reserved.
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.txt
  * @author Cliff Ingham <inghamn@bloomington.in.gov>
  * @param GET documentType_id
  */
-	verifyUser(array('Administrator','Webmaster'));
+verifyUser(array('Administrator','Webmaster'));
 
-	if (isset($_GET['documentType_id'])) { $documentType = new DocumentType($_GET['documentType_id']); }
-	if (isset($_POST['documentType']))
+if (isset($_GET['documentType_id'])) { $documentType = new DocumentType($_GET['documentType_id']); }
+if (isset($_POST['documentType']))
+{
+	$documentType = new DocumentType($_POST['documentType_id']);
+	foreach($_POST['documentType'] as $field=>$value)
 	{
-		$documentType = new DocumentType($_POST['documentType_id']);
-		foreach($_POST['documentType'] as $field=>$value)
-		{
-			$set = "set".ucfirst($field);
-			$documentType->$set($value);
-		}
-
-		try
-		{
-			$documentType->save();
-			Header("Location: home.php");
-			exit();
-		}
-		catch (Exception $e) { $_SESSION['errorMessages'][] = $e; }
+		$set = "set".ucfirst($field);
+		$documentType->$set($value);
 	}
+	$documentInfoFields = array();
+	foreach($_POST['documentInfoFields'] as $field=>$order)
+	{
+		if ($order) { $documentInfoFields[$order] = $field; }
+	}
+	$documentType->setDocumentInfoFields($documentInfoFields);
 
-	$template = new Template();
-	$template->blocks[] = new Block('documentTypes/updateDocumentTypeForm.inc',array('documentType'=>$documentType));
-	echo $template->render();
-?>
+	try
+	{
+		$documentType->save();
+		Header("Location: home.php");
+		exit();
+	}
+	catch (Exception $e) { $_SESSION['errorMessages'][] = $e; }
+}
+
+$template = new Template();
+$template->blocks[] = new Block('documentTypes/updateDocumentTypeForm.inc',array('documentType'=>$documentType));
+echo $template->render();
