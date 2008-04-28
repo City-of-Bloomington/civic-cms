ca3aa56ecff37f0e2303434a22ddfafc0d257020
inghamn <inghamn@07107aec-fef2-11de-b9d1-2d015cf73129>
2008-04-28 14:18:58 +0000

    Bug Fix: Handled invalid facets being passed in the URL
    git-svn-id: https://civic-cms.googlecode.com/svn/trunk@883 07107aec-fef2-11de-b9d1-2d015cf73129

--- html/facets/viewFacet.php
+++ html/facets/viewFacet.php
@@ -1,15 +1,25 @@
 <?php
 /**
- * @copyright Copyright (C) 2007 City of Bloomington, Indiana. All rights reserved.
+ * @copyright Copyright (C) 2007-2008 City of Bloomington, Indiana. All rights reserved.
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.txt
  * @author Cliff Ingham <inghamn@bloomington.in.gov>
  * @param GET facet_id
  */
-	$facet = new Facet($_GET['facet_id']);
+if (isset($_GET['facet_id']) && $_GET['facet_id'])
+{
+	try { $facet = new Facet($_GET['facet_id']); }
+	catch(Exception $e) { $_SESSION['errorMessages'][] = $e; }
+}
 
+if (isset($facet))
+{
 	$template = new Template();
 	$template->blocks[] = new Block('facets/breadcrumbs.inc',array('facet'=>$facet));
 	$template->blocks[] = new Block('facets/facetDocuments.inc',array('facet'=>$facet));
 
 	echo $template->render();
-?>
+}
+else
+{
+	Header('Location: '.BASE_URL.'/facets');
+}
