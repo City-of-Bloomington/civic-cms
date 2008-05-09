2668e1679aa7ea52a9e71942a841f44ffda0fa28
inghamn <inghamn@07107aec-fef2-11de-b9d1-2d015cf73129>
2008-05-09 18:55:17 +0000

    Updated the 404 to fix the non-searching that was happening.  It now just includes the search home.php.  So the code to do the search is all in one place.
    git-svn-id: https://civic-cms.googlecode.com/svn/trunk@893 07107aec-fef2-11de-b9d1-2d015cf73129

--- html/404.php
+++ html/404.php
@@ -1,56 +1,48 @@
 <?php
 /**
- * @copyright Copyright (C) 2007 City of Bloomington, Indiana. All rights reserved.
+ * @copyright Copyright (C) 2007-2008 City of Bloomington, Indiana. All rights reserved.
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.txt
  * @author Cliff Ingham <inghamn@bloomington.in.gov>
  */
- 	$u = new URL($_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']);
-	$path = str_replace(BASE_URL.'/','',$u->getURL());
-	$url = urldecode($path);
- 	$url = explode('?',$url);
+$u = new URL($_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']);
+$path = str_replace(BASE_URL.'/','',$u->getURL());
+$url = urldecode($path);
+$url = explode('?',$url);
 
- 	$title = $url[0];
- 	if (substr($title,-1)=='/') { $title = substr($title,0,-1); }
-	$wikiTitle = WikiMarkup::wikify($title);
+$title = $url[0];
+if (substr($title,-1)=='/') { $title = substr($title,0,-1); }
+$wikiTitle = WikiMarkup::wikify($title);
 
-	$list = new DocumentList(array('wikiTitle_or_alias'=>$wikiTitle));
-	switch (count($list))
-	{
-		case 0:
-			FileNotFoundLog::log($path);
+$list = new DocumentList(array('wikiTitle_or_alias'=>$wikiTitle));
+$count = count($list);
+switch (count($list))
+{
+	case 0:
+		FileNotFoundLog::log($path);
+		$_SESSION['errorMessages'][] = new Exception('404');
 
-			$string = str_replace('-',' ',$wikiTitle);
-			$template = new Template();
-			$template->blocks[] = new Block('404.inc');
-			try
+		# The Search page expects a search param to be passed in the URL
+		$_GET['search'] = preg_replace('/[^a-zA-Z]/',' ',$title);
+		include APPLICATION_HOME.'/html/search/home.php';
+	break;
+
+	case 1:
+		if (isset($url[1]))
+		{
+			$params = explode(';',$url[1]);
+			foreach($params as $param)
 			{
-				$search = new Search();
-				$results = $search->find($string,null,false);
-
-				$template->blocks[] = new Block('search/searchForm.inc',array('search'=>$string));
-				$template->blocks[] = new Block('search/results.inc',array('results'=>$results));
+				$param = explode('=',$param);
+				if (count($param)==2) { $_GET[$param[0]] = $param[1]; }
 			}
-			catch (Exception $e) { exception_handler($e); }
-			echo $template->render();
-		break;
+		}
+		$document = $list[0];
+		$_GET['document_id'] = $document->getId();
+		include APPLICATION_HOME.'/html/documents/viewDocument.php';
+	break;
 
-		case 1:
-			if (isset($url[1]))
-			{
-				$params = explode(';',$url[1]);
-				foreach($params as $param)
-				{
-					$param = explode('=',$param);
-					if (count($param)==2) { $_GET[$param[0]] = $param[1]; }
-				}
-			}
-			$document = $list[0];
-			$_GET['document_id'] = $document->getId();
-			include APPLICATION_HOME.'/html/documents/viewDocument.php';
-		break;
-
-		default:
-			$template = new Template();
-			$template->blocks[] = new Block('search/results.inc',array('results'=>$list));
-			echo $template->render();
-	}
+	default:
+		$template = new Template();
+		$template->blocks[] = new Block('search/results.inc',array('results'=>$list));
+		echo $template->render();
+}
