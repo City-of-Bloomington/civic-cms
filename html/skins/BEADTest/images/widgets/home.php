559aa69b94c6ad129a478290762bf6c5b8d7a0af
inghamn <inghamn@07107aec-fef2-11de-b9d1-2d015cf73129>
2008-12-03 16:16:47 +0000

    Created the BEADTest skin, so we don't lose changes to it.
    git-svn-id: https://civic-cms.googlecode.com/svn/trunk@1030 07107aec-fef2-11de-b9d1-2d015cf73129

---
+++ html/skins/BEADTest/images/widgets/home.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @copyright Copyright (C) 2006,2007 City of Bloomington, Indiana. All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.txt
+ * @author Cliff Ingham <inghamn@bloomington.in.gov>
+ */
+	verifyUser(array('Administrator','Webmaster'));
+
+	$template = new Template();
+
+	$widgets = new WidgetInstallationList();
+	$widgets->find();
+
+	$template->blocks[] = new Block('widgets/widgetList.inc',array('widgetInstallationList'=>$widgets));
+
+	echo $template->render();
+?>
