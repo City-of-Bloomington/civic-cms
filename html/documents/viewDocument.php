a2a1a55b3ad9b07957f16f56dbe77a9ede99b7a2
inghamn <inghamn@07107aec-fef2-11de-b9d1-2d015cf73129>
2009-10-26 19:01:04 +0000

    Fixed error:  Displaying a search for similar documents was failing.  This is the search in question was the search displayed when a user asked for an unpublished document.
    git-svn-id: https://civic-cms.googlecode.com/svn/trunk@1162 07107aec-fef2-11de-b9d1-2d015cf73129

--- html/documents/viewDocument.php
+++ html/documents/viewDocument.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @copyright Copyright (C) 2006,2007 City of Bloomington, Indiana. All rights reserved.
+ * @copyright 2006-2009 City of Bloomington, Indiana
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.txt
  * @author Cliff Ingham <inghamn@bloomington.in.gov>
  * @param GET document_id
@@ -12,172 +12,189 @@
  * Documents that include forms are expected to have the PHP code
  * inside themselves to process their own POST
  */
-	$format = isset($_GET['format']) ? strtolower($_GET['format']) : 'html';
-	switch($format)
-	{
-		case 'print': $template = new Template('print','html'); break;
-		default: $template = new Template();
+$format = isset($_GET['format']) ? strtolower($_GET['format']) : 'html';
+switch($format) {
+	case 'print':
+		$template = new Template('print','html');
+		break;
+	default:
+		$template = new Template();
+}
+
+try {
+	if (isset($_GET['document_id']) && $_GET['document_id']) {
+		$document = new Document($_GET['document_id']);
+	}
+	elseif (isset($_POST['document_id']) && $_POST['document_id']) {
+		$document = new Document($_POST['document_id']);
+	}
+	else {
+		$document = new Document(1);
+	}
+}
+catch (Exception $e) {
+	$_SESSION['errorMessages'][] = $e;
+}
+
+if (isset($document)) {
+	$document->logAccess();
+
+	$template->document = $document;
+	$template->title = $document->getTitle();
+
+	#------------------------------------------------------------
+	# Set up the BreadCrumbs
+	#------------------------------------------------------------
+	$p = isset($_SESSION['previousSectionId']) ? $_SESSION['previousSectionId'] : null;
+	if (isset($_GET['section_id'])) {
+		$s = $_GET['section_id'];
+		$_SESSION['previousSectionId'] = $s;
+	}
+	else {
+		$s = null;
+	}
+	$currentAncestors = $document->getBreadcrumbs($s,$p);
+	$template->currentAncestors = $currentAncestors;
+
+	# Don't display the breadcrumbs on the homepage
+	if ($document->getId() != 1) {
+		$breadcrumbs = new Block('documents/breadcrumbs.inc');
+		$breadcrumbs->document = $document;
+		$breadcrumbs->currentAncestors = $currentAncestors;
+		$template->blocks[] = $breadcrumbs;
 	}
 
-	try
-	{
-		if (isset($_GET['document_id']) && $_GET['document_id']) { $document = new Document($_GET['document_id']); }
-		elseif (isset($_POST['document_id']) && $_POST['document_id']) { $document = new Document($_POST['document_id']); }
-		else { $document = new Document(1); }
+	# The current section should be pulled from the URL
+	# If there isn't one in the URL, try pulling it from $currentAncestors
+	if (isset($_GET['section_id'])) {
+		try {
+			$currentSection = new Section($_GET['section_id']);
+		}
+		catch(Exception $e)
+		{
+			if (count($currentAncestors)) {
+				$currentSection = end($currentAncestors);
+			}
+		}
 	}
-	catch (Exception $e) { $_SESSION['errorMessages'][] = $e; }
+	elseif(count($currentAncestors)) {
+		$currentSection = end($currentAncestors);
+	}
 
-	if (isset($document))
-	{
-		$document->logAccess();
-
-		$template->document = $document;
-		$template->title = $document->getTitle();
-
-		#------------------------------------------------------------
-		# Set up the BreadCrumbs
-		#------------------------------------------------------------
-		$p = isset($_SESSION['previousSectionId']) ? $_SESSION['previousSectionId'] : null;
-		if (isset($_GET['section_id']))
-		{
-			$s = $_GET['section_id'];
-			$_SESSION['previousSectionId'] = $s;
-		}
-		else { $s = null; }
-		$currentAncestors = $document->getBreadcrumbs($s,$p);
-		$template->currentAncestors = $currentAncestors;
-
-		# Don't display the breadcrumbs on the homepage
-		if ($document->getId() != 1) {
-			$breadcrumbs = new Block('documents/breadcrumbs.inc');
-			$breadcrumbs->document = $document;
-			$breadcrumbs->currentAncestors = $currentAncestors;
-			$template->blocks[] = $breadcrumbs;
-		}
-
-		# The current section should be pulled from the URL
-		# If there isn't one in the URL, try pulling it from $currentAncestors
-		if (isset($_GET['section_id']))
-		{
-			try { $currentSection = new Section($_GET['section_id']); }
-			catch(Exception $e)
-			{
-				if (count($currentAncestors)) { $currentSection = end($currentAncestors); }
-			}
-		}
-		elseif(count($currentAncestors)) { $currentSection = end($currentAncestors); }
-
-		#------------------------------------------------------------
-		# Create the panel for alerts
-		#------------------------------------------------------------
-		$template->blocks[] = new Block('alerts/alertPanel.inc',array('document'=>$document));
+	#------------------------------------------------------------
+	# Create the panel for alerts
+	#------------------------------------------------------------
+	$template->blocks[] = new Block('alerts/alertPanel.inc',array('document'=>$document));
 
 
-		#------------------------------------------------------------
-		# Set up the content of the document
-		#------------------------------------------------------------
-		$viewDocument = new Block('documents/viewDocument.inc');
-		$viewDocument->document = $document;
-		if (isset($currentSection)) { $viewDocument->section = $currentSection; }
+	#------------------------------------------------------------
+	# Set up the content of the document
+	#------------------------------------------------------------
+	$viewDocument = new Block('documents/viewDocument.inc');
+	$viewDocument->document = $document;
+	if (isset($currentSection)) {
+		$viewDocument->section = $currentSection;
+	}
 
-		if (!$document->isActive())
-		{
-			$template->blocks[] = new Block('documents/unavailable.inc',array('document'=>$document));
+	if (!$document->isActive()) {
+		$template->blocks[] = new Block('documents/unavailable.inc',array('document'=>$document));
 
-			if (isset($_SESSION['USER']) && $document->permitsEditingBy($_SESSION['USER']))
-			{
-				$template->blocks[] = $viewDocument;
-			}
-			else
-			{
-				try
-				{
-					$search = new Search();
-					$results = $search->find($document->getTitle());
-				}
-				catch (Exception $e) { exception_handler($e); }
-
-
-				$currentType = isset($_GET['type']) ? Inflector::pluralize($_GET['type']) : 'Documents';
-				$type = strtolower($currentType);
-
-
-				if (isset($results[$type]) && count($results[$type]))
-				{
-					# If we've got a lot of results, split them up into seperate pages
-					if ($results[$type] > 10)
-					{
-						$resultArray = new ArrayObject($results[$type]);
-						$pages = new Paginator($resultArray,10);
-
-						# Make sure we're asking for a page that actually exists
-						$page = (isset($_GET['page']) && $_GET['page']) ? (int)$_GET['page'] : 0;
-						if (!$pages->offsetExists($page)) { $page = 0; }
-
-						$resultsList = new LimitIterator($resultArray->getIterator(),$pages[$page],$pages->getPageSize());
-					}
-					else { $resultsList = $this->results[$type]; }
-				}
-				else { $resultsList = array(); }
-				if (isset($results))
-				{
-					$resultsTab = new Block('search/resultTabs.inc');
-					$resultsTab->currentType = $currentType;
-					$resultsTab->type = $type;
-					$resultsTab->results = $results;
-
-					$resultBlock = new Block('search/results.inc');
-					$resultBlock->results = $resultsList;
-					$resultBlock->currentType = $currentType;
-					$resultBlock->type = $type;
-
-					$template->blocks[] = $resultsTab;
-					$template->blocks[] = $resultBlock;
-
-					if (isset($pages))
-					{
-						$pageNavigation = new Block('pageNavigation.inc');
-						$pageNavigation->page = $page;
-						$pageNavigation->pages = $pages;
-						$pageNavigation->url = new URL($_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']);
-
-						$template->blocks[] = $pageNavigation;
-					}
-				}
-			}
-		}
-		else
-		{
+		if (isset($_SESSION['USER']) && $document->permitsEditingBy($_SESSION['USER'])) {
 			$template->blocks[] = $viewDocument;
 		}
-
-
-		# If we're viewing the homepage of the current section
-		foreach($document->getHomeSections() as $section)
-		{
-			# Check for Featured Documents in this Section
-			$types = new DocumentTypeList();
-			$types->find();
-			foreach($types as $type)
-			{
-				$documentList = new DocumentList(array('documentType_id'=>$type->getId(),'section_id'=>$section->getId(),'featured'=>1,'active'=>date('Y-m-d')));
-				if (count($documentList))
-				{
-					$featuredDocuments = new Block('sections/featuredDocuments.inc');
-					$featuredDocuments->documentType = $type;
-					$featuredDocuments->documentList = $documentList;
-					$featuredDocuments->section = $section;
-
-					$template->blocks[] = $featuredDocuments;
-				}
+		else {
+			try {
+				$search = new Search();
+				$results = $search->find($document->getTitle());
+			}
+			catch (Exception $e) {
+				exception_handler($e);
 			}
 
-			# We don't want to show this box on the homepage for now
-			if ($section->getId() != 1)
-			{
-				$template->blocks[] = new Block('sections/documents.inc',array('section'=>$section,'document'=>$document));
+			$currentType = isset($_GET['type']) ? Inflector::pluralize($_GET['type']) : 'Documents';
+
+			if (isset($results[$currentType]) && count($results[$currentType])) {
+				# If we've got a lot of results, split them up into seperate pages
+				if ($results[$currentType] > 10) {
+					$resultArray = new ArrayObject($results[$currentType]);
+					$pages = new Paginator($resultArray,10);
+
+					# Make sure we're asking for a page that actually exists
+					$page = (isset($_GET['page']) && $_GET['page']) ? (int)$_GET['page'] : 0;
+					if (!$pages->offsetExists($page)) {
+						$page = 0;
+					}
+
+					$resultsList = new LimitIterator($resultArray->getIterator(),
+													$pages[$page],
+													$pages->getPageSize());
+				}
+				else {
+					$resultsList = $this->results[$currentType];
+				}
+			}
+			else {
+				$resultsList = array();
+			}
+
+			if (isset($results)) {
+				$type = strtolower($currentType);
+
+				$resultsTab = new Block('search/resultTabs.inc');
+				$resultsTab->currentType = $currentType;
+				$resultsTab->type = $type;
+				$resultsTab->results = $results;
+
+				$resultBlock = new Block('search/results.inc');
+				$resultBlock->results = $resultsList;
+				$resultBlock->currentType = $currentType;
+				$resultBlock->type = $type;
+
+				$template->blocks[] = $resultsTab;
+				$template->blocks[] = $resultBlock;
+
+				if (isset($pages)) {
+					$pageNavigation = new Block('pageNavigation.inc');
+					$pageNavigation->page = $page;
+					$pageNavigation->pages = $pages;
+					$pageNavigation->url = new URL($_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']);
+
+					$template->blocks[] = $pageNavigation;
+				}
 			}
 		}
 	}
+	else {
+		$template->blocks[] = $viewDocument;
+	}
 
-	echo $template->render();
+
+	# If we're viewing the homepage of the current section
+	foreach($document->getHomeSections() as $section) {
+		# Check for Featured Documents in this Section
+		$types = new DocumentTypeList();
+		$types->find();
+		foreach($types as $type) {
+			$documentList = new DocumentList(array('documentType_id'=>$type->getId(),
+												'section_id'=>$section->getId(),
+												'featured'=>1,
+												'active'=>date('Y-m-d')));
+			if (count($documentList)) {
+				$featuredDocuments = new Block('sections/featuredDocuments.inc');
+				$featuredDocuments->documentType = $type;
+				$featuredDocuments->documentList = $documentList;
+				$featuredDocuments->section = $section;
+
+				$template->blocks[] = $featuredDocuments;
+			}
+		}
+
+		# We don't want to show this box on the homepage for now
+		if ($section->getId() != 1) {
+			$template->blocks[] = new Block('sections/documents.inc',
+											array('section'=>$section,'document'=>$document));
+		}
+	}
+}
+
+echo $template->render();
