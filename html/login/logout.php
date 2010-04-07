ae9c3e4f379a9bc4fa277ca1237d36c4f27c5bcb
inghamn <inghamn@07107aec-fef2-11de-b9d1-2d015cf73129>
2010-04-07 20:38:44 +0000

    Removed auto-login feature from CAS support
    git-svn-id: https://civic-cms.googlecode.com/svn/trunk@1429 07107aec-fef2-11de-b9d1-2d015cf73129

--- html/login/logout.php
+++ html/login/logout.php
@@ -6,16 +6,5 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.txt
  * @author Cliff Ingham <inghamn@bloomington.in.gov>
  */
-if (isset($_COOKIE[CAS_COOKIE])) {
-	setcookie(CAS_COOKIE,'true',time()-3600,'/',CAS_DOMAIN);
-
-	require_once CAS.'/SimpleCAS/Autoload.php';
-
-	$options = array('hostname'=>CAS_SERVER,'uri'=>CAS_URI);
-	$protocol = new SimpleCAS_Protocol_Version2($options);
-	$client = SimpleCAS::client($protocol);
-	$client->logout(BASE_URL);
-}
-
 session_destroy();
 header('Location: '.BASE_URL);
