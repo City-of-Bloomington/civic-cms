6c32612227cd451988b924db4ebf76804bbe6555
inghamn <inghamn@07107aec-fef2-11de-b9d1-2d015cf73129>
2012-01-27 19:39:51 +0000

    Media info now displays a 404 message for unknown media_ids.
    git-svn-id: https://civic-cms.googlecode.com/svn/trunk@1613 07107aec-fef2-11de-b9d1-2d015cf73129

--- html/media/info.php
+++ html/media/info.php
@@ -1,14 +1,23 @@
 <?php
 /**
- * @copyright Copyright (C) 2007 City of Bloomington, Indiana. All rights reserved.
+ * @copyright 2007-2012 City of Bloomington, Indiana
  * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
  * @author Cliff Ingham <inghamn@bloomington.in.gov>
  * @param GET media_id
  */
-	verifyUser();
-	$media = new Media($_GET['media_id']);
+verifyUser();
 
-	$template = new Template('backend');
+try  {
+	$media = new Media($_GET['media_id']);
+}
+catch (Exception $e) {
+	header('http/1.1 404 Not Found');
+	header('Status: 404 Not Found');
+	$_SESSION['errorMessages'][] = $e;
+}
+
+$template = new Template('backend');
+if (isset($media)) {
 	$template->blocks[] = new Block('media/info.inc',array('media'=>$media));
-	echo $template->render();
-?>
+}
+echo $template->render();
