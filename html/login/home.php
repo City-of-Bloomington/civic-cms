0b2706d6438e73f29e164b95818538f6c55a572d
inghamn <inghamn@07107aec-fef2-11de-b9d1-2d015cf73129>
2012-02-27 14:41:03 +0000

    Switched to phpCAS instead of SimpleCAS
    git-svn-id: https://civic-cms.googlecode.com/svn/trunk@1617 07107aec-fef2-11de-b9d1-2d015cf73129

--- html/login/home.php
+++ html/login/home.php
@@ -2,36 +2,34 @@
 /**
  *	Logs a user into the system using CAS
  *
- * @copyright 2006-2010 City of Bloomington, Indiana
+ * @copyright 2006-2012 City of Bloomington, Indiana
  * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
  * @author Cliff Ingham <inghamn@bloomington.in.gov>
  */
 if (isset($_REQUEST['return_url'])) {
 	$_SESSION['return_url'] = $_REQUEST['return_url'];
 }
-require_once CAS.'/SimpleCAS/Autoload.php';
 
-$options = array('hostname'=>CAS_SERVER,'uri'=>CAS_URI);
-$protocol = new SimpleCAS_Protocol_Version2($options);
-$client = SimpleCAS::client($protocol);
-$client->forceAuthentication();
+$return_url = isset($_SESSION['return_url']) ? $_SESSION['return_url'] : BASE_URL;
 
-if ($client->isAuthenticated()) {
-	try {
-		$user = new User($client->getUsername());
-		$user->startNewSession();
-
-		if (isset($_SESSION['return_url'])) {
-			header('Location: '.$_SESSION['return_url']);
-		}
-		else {
-			header('Location: '.BASE_URL);
-		}
-	}
-	catch (Exception $e) {
-		$_SESSION['errorMessages'][] = $e;
-	}
+// If they don't have CAS configured, send them onto the application's
+// internal authentication system
+if (!defined('CAS')) {
+	header('Location: '.BASE_URL.'/login/login?return_url='.$return_url);
+	exit();
 }
-else {
-	header('Location: '.BASE_URL);
+
+require_once CAS.'/CAS.php';
+phpCAS::client(CAS_VERSION_2_0, CAS_SERVER, 443, CAS_URI, false);
+phpCAS::setNoCasServerValidation();
+phpCAS::forceAuthentication();
+
+try {
+	$user = new User(phpCAS::getUser());
+	$user->startNewSession();
 }
+catch (Exception $e) {
+	$_SESSION['errorMessages'][] = $e;
+}
+
+header("Location: $return_url");
