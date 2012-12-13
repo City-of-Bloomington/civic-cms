97944d89a6d37b455b2ec05cdd9f96a11cea4bd9
inghamn <inghamn@07107aec-fef2-11de-b9d1-2d015cf73129>
2012-12-13 22:01:26 +0000

    Fixed error with local login url
    git-svn-id: https://civic-cms.googlecode.com/svn/trunk@1682 07107aec-fef2-11de-b9d1-2d015cf73129

--- html/login/home.php
+++ html/login/home.php
@@ -15,7 +15,7 @@ $return_url = isset($_SESSION['return_url']) ? $_SESSION['return_url'] : BASE_UR
 // If they don't have CAS configured, send them onto the application's
 // internal authentication system
 if (!defined('CAS')) {
-	header('Location: '.BASE_URL.'/login/login?return_url='.$return_url);
+	header('Location: '.BASE_URL.'/login/login.php?return_url='.$return_url);
 	exit();
 }
 
